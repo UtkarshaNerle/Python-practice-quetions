10b89fc6f31ee428998533687a795e2c60f7aa77
UtkarshaNerle <utkarshanerle@gmail.com>
2024-10-01 13:15:34 +0530

    object attributes > class attributes

---
+++ instance.py
@@ -0,0 +1,12 @@
+class student:
+    college_name = 'MRIT college'
+    name = 'utkarsha'
+    
+    def __init__(self,name ,marks):
+        self.name = name
+        self.marks = marks
+        
+s1 = student('yash', 98)
+print(s1.name,s1. marks) 
+
+print(s1.college_name)
