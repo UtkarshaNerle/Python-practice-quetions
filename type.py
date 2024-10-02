8f60289a4fbf09fed657b27ca9561743271f5d87
UtkarshaNerle <utkarshanerle@gmail.com>
2024-10-02 23:23:59 +0530

    finding from list sum of list

--- type.py
+++ type.py
@@ -1,8 +1,17 @@
-b = [1,4,5,2,1,3,11,7,17]
-i = 1
+b = [1,4,5,'2','1','3',11,7,'17']
+i = 0
 sum = 0
 while i < len(b):
-    if i%2 != 0:
-        sum+=1
+    if type(b[i]) == int:
+        sum+=b[i]
     i+=1   
 print([sum])
+
+# B = [1, 4, 5, '2', '1', '3', 11, 7, '17']
+# i = 0
+# sum = 0
+# while i < len(B):
+#     if type(B[i]) == int:
+#         sum = sum + B[i]
+#     i += 1
+# print([sum])       
