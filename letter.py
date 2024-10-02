b962caff801c7d43e683238a2fe1fbc5d551ad57
UtkarshaNerle <utkarshanerle@gmail.com>
2024-10-02 19:35:30 +0530

    finding a letters from usn

--- letter.py
+++ letter.py
@@ -1,8 +1,8 @@
 b = '4mr24cse#'
 i = 0
-A = 0
-B = 0
-E = 0
+A = ""
+B = ""
+E = ""
 while i < len(b):
     if '0' <= b[i] <= '9':
         A = A+b[i]
@@ -10,7 +10,7 @@ while i < len(b):
         B = B+b[i]
     else:
         E = E+b[i]
-    i+=b[i]
-print('A =',A)
-print('B =',B)
-print('E =',E)
+    i+=1
+print('A (alphabetes) =',A)
+print('B (numbers)=',B)
+print('E (special charchar)=',E)
